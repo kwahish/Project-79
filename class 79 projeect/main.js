19702a16b7f86e88e2c3fd56faa2934c35032f30
kwahish <108003879+kwahish@users.noreply.github.com>
2022-06-28 19:16:35 +0530

    Add files via upload

---
+++ class 79 projeect/main.js
@@ -0,0 +1,17 @@
+menu_list_array = ["Veg Margherita Pizza","Onion and Paneer Pizza","Chicken Pizza","Cheese n Corn Pizza","Cheese n Tomato Pizza"];
+
+function getmenu(){
+var htmldata;
+//Complete the code
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+//Complete the code
+
+}
+
+function add_top(){
+//Complete the code
+}
